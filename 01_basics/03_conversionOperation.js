f2bc979d06cbea301cbe7afd8711efa58e6ffdb5
VIKAS BHONDE <113436990+vikasvs2003@users.noreply.github.com>
2024-11-12 07:29:15 +0000

    ewtgf

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,16 +1,43 @@
 // 03_conversionOperation.js
 
-let score="dfasgd"
-console.log(typeof score);
-console.log(typeof (score));
+// let score="dfasgd"
+// console.log(typeof score);
+// console.log(typeof (score));
 
 
-let valueNumber=Number(score)
-console.log(typeof valueNumber);
-console.log( valueNumber);
+// let valueNumber=Number(score)
+// console.log(typeof valueNumber);
+// console.log( valueNumber);
 
 
 //"33"-> 33
 //"33abc" => nan
 //true ->1
+
+//***************OPERATIONS******************* */
+/*
+
+let VALUE =4
+let negvalue=-VALUE
+
+console.log(negvalue);
+
+
+let str1="vikas"
+let str2 = 4
+
+str3= str1+str2
+console.log(str3);
+
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+3);
+console.log("1"+"2");
+console.log(1+2+"3");
+
+*/
+console.log(+true);
+console.log(+"");
+
 
