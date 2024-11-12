348bd9ca2e65f8032b7bd9fa5e8f680c322380ff
VIKAS BHONDE <113436990+vikasvs2003@users.noreply.github.com>
2024-11-12 08:13:51 +0000

    DATA TYPES UMMARY

---
+++ 01_basics/04_datatypessummary.js
@@ -0,0 +1,10 @@
+//04_datatypessummary.js
+/*
+
+javascript is DYANAIC type language 
+primitive and non-primitive 
+7 types: String,number,boolean,null,undefined,symbol,bigint,
+
+refrence types non-prmitive -  //array.objects,fnuction 
+
+*/
